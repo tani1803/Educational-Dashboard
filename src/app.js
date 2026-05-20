6104d09802d52aaf3482943a571cae4636ce60dd
Abhinav <abhinavgbls@gmail.com>
2026-05-20 08:17:08 +0530

    Version fixup

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.options(/.*/, cors());
 
 const authRoutes = require("./routes/auth.routes");
 const courseRoutes = require("./routes/course.routes");
